30f0c990be5e89ffe928b38471190aaeaf66e5d6
Ivan <ivandodo@gmail.com>
2017-04-25 00:03:42 +0200

    2

--- geocode.py
+++ geocode.py
@@ -1,13 +1,30 @@
-import httplib2
-import json
+#import httplib2
+#import json
+import requests
+from codes import *
 
 def geoCodeLocation(inputString):
-    google_api_key = "AIzaSyCnTdi7mZwbJ0Lup58Z8BcBMyDR3_fxFOI"
     location = inputString.replace(" ","+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (location, google_api_key))
 
-    h = httplib2.Http()
-    response, content = h.request(url, 'GET')
-    print ("response header: %s \n \n" % response)
-    result = json.load(content)
+    content = requests.get(url)
+    result = content.json()
+    if (result['status'] == 'OK'):
+        return result['results'][0]['geometry']['location']
+    else:
+        return None, None
+
+def findARestaurant(mealType, location):
+    latitude, longitude = geoCodeLocation(location)
+    if (latitude == None):
+        return 'No data for %s in %s'%(mealType, location)
+
+    url="""https://api.foursquare.com/v2/venues/search?
+    client_id=%s
+    &client_secret=%s
+    &v=20130815
+    &ll=%s,%s
+    &query=%s"""%(CLIENT_ID,CLIENT_SECRET,latitude, longitude, mealType)
+
+    result = requests.get(url).json()
     return result
