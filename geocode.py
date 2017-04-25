fab4144355368c85c47cde82ff683bea3ae07c3f
ivan.radojevic <ivandodo@gmail.com>
2017-04-25 18:01:09 +0200

    added correct 4sq api

--- geocode.py
+++ geocode.py
@@ -1,6 +1,5 @@
-#import httplib2
-#import json
 import requests
+import json
 from codes import *
 
 def geoCodeLocation(inputString):
@@ -12,19 +11,35 @@ def geoCodeLocation(inputString):
     if (result['status'] == 'OK'):
         return result['results'][0]['geometry']['location']
     else:
-        return None, None
+        return None
+
 
 def findARestaurant(mealType, location):
-    latitude, longitude = geoCodeLocation(location)
-    if (latitude == None):
-        return 'No data for %s in %s'%(mealType, location)
+    loc = geoCodeLocation(location)
+    if not loc:
+        return 'No Google data for %s in %s'%(mealType, location)
 
-    url="""https://api.foursquare.com/v2/venues/search?
-    client_id=%s
-    &client_secret=%s
-    &v=20130815
-    &ll=%s,%s
-    &query=%s"""%(CLIENT_ID,CLIENT_SECRET,latitude, longitude, mealType)
+    url="""https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s"""\
+        %(CLIENT_ID,CLIENT_SECRET,loc['lat'], loc['lng'], mealType)
 
     result = requests.get(url).json()
-    return result
+
+    try:
+        venue = result['response']['venues'][0]
+    except IndexError:
+        return None
+
+    if venue:
+        venue_id = venue['id']
+        url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&v=20150603&client_secret=%s' % (
+        (venue_id, CLIENT_ID, CLIENT_SECRET)))
+        result = (requests.get(url)).json()
+
+        image_ref = result['response']['photos']['items']
+        img = next((s for s in image_ref if s), None)
+        return (img and img['prefix']+"300x300"+img['suffix']) or None
+    else:
+        return 'No data for %s in %s'%(mealType, location)
+
+
+    return result['response']['venues'][0]
